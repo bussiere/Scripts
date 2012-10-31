67f093cc8d4d13d116ac53ee5cbc74d495926bd3
bussiere <bussiere@gmail.com>
2012-10-31 10:14:43 +0100

    rajout de script

---
+++ corde.py
@@ -0,0 +1,8 @@
+pie = 3.14
+pointure = 0.31
+nbrepieds = 6.5
+ajustement = 1.5
+prixcorde = 12
+prixchaine = 7
+print pie * 2 * pointure * nbrepieds  * ajustement * ajustement * prixcorde
+print pie * 2 * pointure * nbrepieds  * ajustement * prixchaine
