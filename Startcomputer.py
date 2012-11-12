bcf8ef2d8146a718ff355cfd25d8aec01b178c06
bussiere <bussiere@gmail.com>
2012-11-12 12:57:54 +0100

    razjout

--- Startcomputer.py
+++ Startcomputer.py
@@ -2,11 +2,13 @@ import os
 import os
 import datetime
 import random
-message = "Shutdown a : "
+message = "start a : "
 now = datetime.datetime.now()
 now = str(now.strftime("%Y-%m-%d %H:%M"))
 os.chdir("C:\\AtelierSODA\\workspaceSODA\\madrhas")
-os.system("echo heure de shtudown %s >> C:\\AtelierSODA\\workspaceSODA\\madrhas\\shutdown.txt"%(now))
+os.system("echo heure de start %s >> C:\\AtelierSODA\\workspaceSODA\\madrhas\\startcxomputer.txt"%(now))
 os.system("git add . >> C:\\AtelierSODA\\logadd.txt")
 os.system("git commit -m '%s %s' >> C:\\AtelierSODA\\logcommit.txt"%(now,message))
-os.system("shutdown -s -t 00")
+os.system("C:\\AtelierSODA\\IDE\\eclipse\\eclipse.exe")
+os.system("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
+os.system("exit")
